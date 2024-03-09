9a9cece7685bfea832b408fc1c68d01cf2431e3f
iauth2024 <161607179+iauth2024@users.noreply.github.com>
2024-03-09 21:06:06 +0530

    Update settings.py

--- idara/settings.py
+++ idara/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&0gi-u$@8=0v*m2bo29r31prpkr0uqi=8r@6h7ztuh-*nyg0nt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
